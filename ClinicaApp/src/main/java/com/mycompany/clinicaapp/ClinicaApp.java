ea5d40ae73c00d6d9abe40521b944ed42e2e1cda
jeromar <jmartinez.carabali@udea.edu.co>
2025-10-13 16:39:17 -0500

    Creación del proyecto en Netbeans

---
+++ ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicaapp;
+
+/**
+ *
+ * @author jmart
+ */
+public class ClinicaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
