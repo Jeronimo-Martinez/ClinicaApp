eaff5ddd68c522992d542138ba90da8ef9221233
hectordcelya <hectord.celya@udea.edu.co>
2025-10-14 15:05:24 -0500

    fixing syntax errors

---
+++ ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clinicaapp;
+
+/**
+ *
+ * @author hecto
+ */
+public class ClinicaApp {
+    public static void main(String[] args) {
+    System.out.println("Iniciando sistema de la clinica...");
+}
+}
