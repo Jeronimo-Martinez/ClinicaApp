e525f3234b53fee2cf8b8078f5fe35fe88e5a011
hectordcelya <hectord.celya@udea.edu.co>
2025-10-14 12:35:55 -0500

    create initial structure and project packages

--- ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
+++ ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
@@ -8,9 +8,9 @@ package com.mycompany.clinicaapp;
  *
  * @author jmart
  */
-public class ClinicaApp {
 
+public class ClinicaApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Iniciando sistema de la clinica...");
     }
 }
