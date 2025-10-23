ef446622c6d7fb1ce39f5f6eb8e5664ac96d9873
jeromar <jmartinez.carabali@udea.edu.co>
2025-10-22 19:08:07 -0500

    Revert "I have no idea what a 'merge' is"
    This reverts commit fdb952ad89963a0ad3bcfc48784f9130b88ec1c7.

--- ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
+++ ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
@@ -4,11 +4,25 @@
  */
 package com.mycompany.clinicaapp;
 
+import com.mycompany.clinicaapp.Presentacion.VentanaPrincipal;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 
 /**
  *
  * @author hecto
  */
 public class ClinicaApp {
+    private static VentanaPrincipal main;
+    public static void main(String[] args) {
+    main = new VentanaPrincipal();
+    main.setVisible(true);
+    Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (pantalla.width - main.getWidth()) / 2;
+    int y = (pantalla.height - main.getHeight()) / 2;
+
+    main.setLocation(x, y);
     
+}
 }
