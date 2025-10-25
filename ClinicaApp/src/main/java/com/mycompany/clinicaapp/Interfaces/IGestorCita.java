5f3d84f07d933bee94229001c3bdf0da6006e7fd
vflorezz <vflz.625@gmail.com>
2025-10-25 11:29:53 -0500

    Corregir package en IGestorCita y en IHistorialService

--- ClinicaApp/src/main/java/com/mycompany/clinicaapp/Interfaces/IGestorCita.java
+++ ClinicaApp/src/main/java/com/mycompany/clinicaapp/Interfaces/IGestorCita.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.mycompany.clinicaapp.LogicaDelNegocio;
+package com.mycompany.clinicaapp.Interfaces;
 
 import java.util.List;
 import com.mycompany.clinicaapp.Modelos.Paciente;
