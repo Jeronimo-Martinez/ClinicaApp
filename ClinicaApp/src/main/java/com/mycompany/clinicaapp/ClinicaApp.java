1abdc96e3bc54aee4975d9e972cf21be323b41ab
jhoandaza <jhoan.daza@udea.edu.co>
2025-10-26 11:34:01 -0500

    Más cosas para que el merge no sea tan conflictivo

--- ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
+++ ClinicaApp/src/main/java/com/mycompany/clinicaapp/ClinicaApp.java
@@ -4,28 +4,14 @@
  */
 package com.mycompany.clinicaapp;
 
-import com.mycompany.clinicaapp.LogicaDelNegocio.GestorMedico;
-import com.mycompany.clinicaapp.Interfaces.IInterfazAdminMedica;
-import com.mycompany.clinicaapp.Interfaces.IMedicoService;
-import com.mycompany.clinicaapp.LogicaDelNegocio.InterfazAdminMedica;
-import javax.swing.SwingUtilities;
-
 
 /**
  *
  * @author hecto
  */
 public class ClinicaApp {
-    public static void main(String[] args){
-     SwingUtilities.invokeLater(() -> {
-            // Instancia del servicio de médicos (puede ser tu GestorMedico singleton o una implementación)
-            IMedicoService medicoService = GestorMedico.getInstancia();
+   
 
-            // Crea la interfaz de administración y la muestra
-            IInterfazAdminMedica interfazAdmin = new InterfazAdminMedica(medicoService);
-            interfazAdmin.mostrarVentanaMedica();
-        });
     
     
-    }
 }
